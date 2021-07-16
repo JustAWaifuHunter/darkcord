e371262b0d865d61056105edd252ee56bd43f0fa
Brian Rhudy <hcsbrian4056@gmail.com>
2021-07-16 00:02:56 -0300

    Loader Fixed

--- lib/src/structures/command/Loader.ts
+++ lib/src/structures/command/Loader.ts
@@ -1,41 +1,52 @@
 import fs from 'fs'
 import path from 'path'
+import { cpuUsage } from 'process'
 import type Client from '../../Client'
 import isClass from '../../util/isClass'
 import isConstructor from '../../util/isConstructor'
 
-export async function CommandLoader (client: Client, _path: string = 'darkcord/commands') {
+export async function CommandLoader (client: Client, _path: string = './darkcord/commands') {
   const commandsLoaded = []
 
-  const dir = `${path.dirname(<string>require.main?.filename)}${path.sep}${_path}`
+  const dir = path.join(path.dirname(<string>require.main?.filename), _path)
 
-  fs.readdir(path.resolve(_path), (err, __) => {
+  fs.readdir(path.resolve(dir), (err, __) => {
     if (err) throw err
     for (const pof of __) {
-      if (fs.lstatSync(path.join(_path, pof)).isDirectory()) {
+      if (fs.lstatSync(path.join(dir, pof)).isDirectory()) {
         fs.readdir(path.join(_path, pof), (err, files) => {
           if (err) throw err
 
           files = files.filter(file => file.endsWith('.js'))
           for (const file of files) {
-            const _dir = path.relative(dir, file)
+            const _dir = path.relative(
+              path.join(dir, file),
+              path.join(_path, file)
+            )
             let commando = require(_dir)
 
-            if (isClass(commando)) {
-              if (commando.default) {
-                commando = commando.default
-              }
+            if (commando.default) {
+              commando = commando.default
+            }
 
+            if (isClass(commando)) {
               if (isConstructor(commando)) {
                 commando = new commando()
-                if (commando.name) {
-                  client.commands.set(commando.name, commando)
+
+                const name = commando.options?.name ?? commando.name
+
+                if (name) {
+                  commandsLoaded.push(name)
+                  client.commands.set(name, commando)
                 } else {
                   throw new Error(`Missing command name.\nFile: ${file}`)
                 }
               } else {
-                if (commando.name) {
-                  client.commands.set(commando.name, commando)
+                const name = commando.options?.name ?? commando.name
+
+                if (name) {
+                  commandsLoaded.push(name)
+                  client.commands.set(name, commando)
                 } else {
                   throw new Error(`Missing command name.\nFile: ${file}`)
                 }
@@ -47,24 +58,35 @@ export async function CommandLoader (client: Client, _path: string = 'darkcord/c
         })
       } else {
         if (pof.endsWith('.js')) {
-          const _dir = path.relative(dir, pof)
+          const _dir = path.relative(
+            path.join(dir, pof),
+            path.join(_path, pof)
+          )
+
           let commando = require(_dir)
 
-          if (isClass(commando)) {
-            if (commando.default) {
-              commando = commando.default
-            }
+          if (commando.default) {
+            commando = commando.default
+          }
 
+          if (isClass(commando)) {
             if (isConstructor(commando)) {
               commando = new commando()
-              if (commando.name) {
-                client.commands.set(commando.name, commando)
+
+              const name = commando.options?.name ?? commando.name
+
+              if (name) {
+                commandsLoaded.push(name)
+                client.commands.set(name, commando)
               } else {
                 throw new Error(`Missing command name.\nFile: ${pof}`)
               }
             } else {
-              if (commando.name) {
-                client.commands.set(commando.name, commando)
+              const name = commando.options?.name ?? commando.name
+
+              if (name) {
+                commandsLoaded.push(name)
+                client.commands.set(name, commando)
               } else {
                 throw new Error(`Missing command name.\nFile: ${pof}`)
               }
